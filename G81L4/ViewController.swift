3944b17e94547f964a2a5336a2bb2981b788561c
Ivan Besarab <IvanVBesarab@gmail.com>
2020-08-13 19:31:20 +0300

    Initial commit

---
+++ G81L4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G81L4
+//
+//  Created by Ivan Vasilevich on 8/13/20.
+//  Copyright © 2020 ivan.besarab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
